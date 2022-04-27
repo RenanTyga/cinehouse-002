2a2a2a2e33b32b8a61a14c08594e53c63f95b53f
RenanTyga <renan.rzprodutora@gmail.com>
2022-04-27 01:37:04 -0300

    alterarStatusEmCartaz

--- cinema.js
+++ cinema.js
@@ -7,10 +7,35 @@ var catalogoObj = JSON.parse(catalogoString);
 
 function listarTodosOsFilmes(filmes) {
   for (let i = 0; i < filmes.length; i++) {
-    console.log(filmes[i].titulo);
+    // console.log(filmes[i].titulo);
   }
 }
 listarTodosOsFilmes(catalogoObj.data);
 
+function listarFilmesEmCartaz(filmes) {
+  const filmesEmCartaz = filmes.filter((filme) => {
+    if (filme.emCartaz === true) {
+      return true;
+    }
+  });
+  return filmesEmCartaz;
+}
+// console.log(listarFilmesEmCartaz(catalogoObj.data));
+
+function alterarStatusEmCartaz(id, filmes) {
+  const idxFilme = filmes.findIndex((filme) => {
+    if (filme.codigo === id) {
+      return true
+    }
+  });
+  if (idxFilme >= 0) {
+    filmes[idxFilme].emCartaz = !filmes[idxFilme].emCartaz;
+    return true;
+  } else {
+    return false;
+  }}
+
+console.log(alterarStatusEmCartaz(1, catalogoObj.data));
+console.log(catalogoObj.data);
 
 
